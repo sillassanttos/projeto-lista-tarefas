e981077e1fd0130ab82ecc635e5202e9fa4bd19a
Silas Santos <sillassanttos@outlook.com>
2023-03-05 16:01:17 -0400

    Adicionado classe de conexao com mysql

---
+++ private/conexao.php
@@ -0,0 +1,30 @@
+<?php
+
+  class Conexao {
+
+    private $host = 'localhost';
+    private $dbname = 'tarefas';
+    private $user = 'root';
+    private $pass = 'root';
+
+    public function conectar() {
+
+      try {
+
+        $conexao = new PDO(
+          "mysql:host=$this->host;dbname=$this->dbname",
+          "$this->user",
+          "$this->pass"
+        );
+
+        return $conexao;
+
+      } catch(PDOException $e) {
+        echo '<p>' . $e->getMessage() . '</p>';
+      }
+
+    }
+
+  }
+
+?>
