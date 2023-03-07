377df6617850140ceee19c327724c585617e6aa6
Silas Santos <sillassanttos@outlook.com>
2023-03-06 20:44:00 -0400

    Inserindo tarefas

--- private/tarefa.service.php
+++ private/tarefa.service.php
@@ -2,8 +2,19 @@
 
   class TarefaService {
 
-    public function inserir() {
+    private $conexao;
+    private $tarefa;
 
+    public function __construct(Conexao $conexao, Tarefa $tarefa) {
+      $this->conexao = $conexao->conectar();
+      $this->tarefa = $tarefa;
+    }
+
+    public function inserir() {
+      $query = 'insert into tb_tarefas(tarefa) values(:tarefa)';
+      $stmt = $this->conexao->prepare($query);
+      $stmt->bindValue(':tarefa', $this->tarefa->__get('tarefa'));
+      $stmt->execute();
     }
 
     public function recuperar() {
--- private/tarefa_controller.php
+++ private/tarefa_controller.php
@@ -1,7 +1,34 @@
 <?php
 
-  echo '<pre>';
-  print_r($_POST);
-  echo '</pre>';
+  /*
+
+    Verificar o contexto.
+
+    Caso esses arquivo estiverem em uma pasta fora da pasta public
+    eles deven ser acessado via contexto.
+    Exemplo:
+
+    > xamp
+    > > htdocs
+    > > > lista-tarefas
+    > > lista-tarefas
+        require "../../lista-tarefas/tarefa.model.php";
+        require "../../lista-tarefas/tarefa.service.php";
+        require "../../lista-tarefas/conexao.php";
+  */
+
+  require "tarefa.model.php";
+  require "tarefa.service.php";
+  require "conexao.php";
+
+  $tarefa = new Tarefa();
+  $tarefa->__set('tarefa', $_POST['tarefa']);
+
+  $conexao = new Conexao();
+
+  $tarefaService = new TarefaService($conexao, $tarefa);
+  $tarefaService->inserir();
+
+  header('Location: nova_tarefa.php?inclusao=1');
 
 ?>
