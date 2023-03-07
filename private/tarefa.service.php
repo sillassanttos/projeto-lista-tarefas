9e1b46771e3fec5b4e85f98495db9af07dbe8931
Silas Santos <sillassanttos@outlook.com>
2023-03-06 21:07:39 -0400

    Consultando todas as tarefas

--- private/tarefa.service.php
+++ private/tarefa.service.php
@@ -18,7 +18,18 @@
     }
 
     public function recuperar() {
-
+      $query = '
+        select t.id
+             , s.status
+             , t.tarefa
+          from tb_tarefas t
+          left
+          join tb_status s
+            on t.id_status = s.id
+      ';
+      $stmt = $this->conexao->prepare($query);
+      $stmt->execute();
+      return $stmt->fetchAll(PDO::FETCH_OBJ);
     }
 
     public function atualizar() {
